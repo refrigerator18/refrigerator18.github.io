40f3e4f98e7c12880088beff7e879dbe4395b299
Huzaifa <zar@ualberta.ca>
2020-08-23 09:53:28 -0600

    second commit

---
+++ static/nice_button.js
@@ -0,0 +1,9 @@
+document.querySelector('#nice_button').onmouseover = function() {
+    document.querySelector('#nice_button').style.backgroundColor = '#33332d'
+    document.querySelector('#nice_button').style.color = 'white'
+            }
+
+document.querySelector('#nice_button').onmouseout = function() {
+    document.querySelector('#nice_button').style.backgroundColor = 'initial'
+    document.querySelector('#nice_button').style.color = 'initial'
+            }
